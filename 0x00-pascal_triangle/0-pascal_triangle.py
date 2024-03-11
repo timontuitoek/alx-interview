1bd50508296089dfb88092457baed0b21bad9370
timontuitoek <tuitoektimon@gmail.com>
2024-03-11 14:06:16 +0300

    Adding shebang

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,14 +5,27 @@ def pascal_triangle(n)
 
 
 def pascal_triangle(n):
+    """Returns a list of lists of integers
+    representing the Pascal’s triangle of n:
+    """
+
     if n <= 0:
         return []
 
-    triangle = []
-    for i in range(n):
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
+    """ initialize an empty resulting array """
+    pascal_list = [[] for idx in range(n)]
 
-    return triangle
+    for li in range(n):
+        for col in range(li+1):
+            if (col < li):
+                if (col == 0):
+                    """ the first column is always set to 1 """
+                    pascal_list[li].append(1)
+                else:
+                    pascal_list[li].append(
+                        pascal_list[li-1][col] + pascal_list[li-1][col-1])
+            elif (col == li):
+                """ the diagonal is always set to 1 """
+                pascal_list[li].append(1)
+
+    return pascal_list
