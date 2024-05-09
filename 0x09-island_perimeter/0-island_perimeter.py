c1f321a52d5b0879fcc4bc9c96c255822bef602c
timontuitoek <tuitoektimon@gmail.com>
2024-05-10 00:08:40 +0300

    Island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
+"""Island perimeter.
+"""
+
 
 def island_perimeter(grid):
-    """
-    Calculates the perimeter of the island described in grid.
+    """Computes the perimeter of an island with no lakes.
     """
     perimeter = 0
-
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:  # If current cell is land
-                perimeter += 4  # Start with assuming 4 sides
-
-                # Check adjacent cells
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Deduct 2 if there's land above
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Deduct 2 if there's land to the left
-
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
-
-
-# Example usage:
-grid = [
-    [0, 1, 0, 0],
-    [1, 1, 1, 0],
-    [0, 1, 0, 0],
-    [1, 1, 0, 0]
-]
-print(island_perimeter(grid))  # Output should be 12
